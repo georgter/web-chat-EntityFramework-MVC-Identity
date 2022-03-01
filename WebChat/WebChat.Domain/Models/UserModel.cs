df9ceb0ffa34b7cf83c86b2ce02f3da45c119489
georgter <83006668+georgter@users.noreply.github.com>
2022-03-01 10:25:50 +0600

    Create-UserModel

--- WebChat/WebChat.Domain/Models/UserModel.cs
+++ WebChat/WebChat.Domain/Models/UserModel.cs
@@ -6,5 +6,9 @@ namespace WebChat.Domain.Models
 {
     class UserModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Pass { get; set; }
     }
 }
